79092660b4b5b7a3beffac14fba1aa78c1f32afa
bassem dhouieb <bassemdwb@gmail.com>
2021-11-28 22:35:03 +0100

    final commit

---
+++ main.py
@@ -0,0 +1,49 @@
+from turtle import Screen, Turtle
+
+from ball import Ball
+from paddle import Paddle
+import time
+
+from scoreboard import ScoreBoard
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Ping Pong Game")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = ScoreBoard()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "z")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    # detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # detect collision with r_paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 400:
+        ball.reset_position()
+        scoreboard.l_point()
+    if ball.xcor() < -400:
+        ball.reset_position()
+        scoreboard.r_point()
+
+screen.exitonclick()
